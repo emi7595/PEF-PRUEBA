b33dc20bbacf7f33a711ac3116087fc833c5f5f4
EduardoGcia <60627778+EduardoGcia@users.noreply.github.com>
2023-09-17 12:31:10 -0600

    test

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ def get_image_as_base64(image_filename):
         base64_image = base64.b64encode(image_data).decode('utf-8')
     return base64_image
 
+""" def get_video_as_base64(video_filename):
+    with open(f"static/videos/{video_filename}", "rb") as video_file:
+        video_data = video_file.read()
+        base64_video = base64.b64encode(video_data).decode('utf-8')
+    return base64_video """
+
 @app.route('/aprende', methods=['GET'])
 def aprende():
     return jsonify(lecciones)
@@ -43,26 +49,29 @@ def getSaludos(id):
     return jsonify({"error": "Saludo no encontrado."}), 404
 
 # 2da Opción
-@app.route('/lecciones/<string:leccion>/<int:id>', methods=['GET'])
-def getLecciones(leccion, id):
-    if leccion == 'abecedario':
+@app.route('/lecciones/<int:id_leccion>/<int:id_seccion>', methods=['GET'])
+def getLecciones(id_leccion, id_seccion):
+    if id_leccion == 1:
         for item in abecedario:
-            if item['id'] == id:
+            if item['id'] == id_seccion:
                 item['imagen'] = get_image_as_base64(item['imagen'])
+                """ item['video'] = get_video_as_base64(item['video_filename']) """
                 return jsonify(item)
         return jsonify({"error": "Letra no encontrada."}), 404
-    elif leccion == 'numeros':
-        for item in numeros:
-            if item['id'] == id:
-                item['imagen'] = get_image_as_base64(item['imagen'])
-                return jsonify(item)
-        return jsonify({"error": "Número no encontrado."}), 404
-    elif leccion == 'saludos':
+    elif id_leccion == 2:
         for item in saludos:
-            if item['id'] == id:
+            if item['id'] == id_seccion:
                 item['imagen'] = get_image_as_base64(item['imagen'])
+                """ item['video'] = get_video_as_base64(item['video_filename']) """
                 return jsonify(item)
         return jsonify({"error": "Saludo no encontrado."}), 404
+    elif id_leccion == 3:
+        for item in numeros:
+            if item['id'] == id_seccion:
+                item['imagen'] = get_image_as_base64(item['imagen'])
+                """ item['video'] = get_video_as_base64(item['video_filename']) """
+                return jsonify(item)
+        return jsonify({"error": "Número no encontrado."}), 404
     else:
         return jsonify({"error": "Lección no encontrada."}), 404
     
